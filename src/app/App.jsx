c5b4214c440a4385785e8fbc612d4cf75eb2d832
shahadat hossen <hossantopu@gmail.com>
2025-05-05 02:12:59 +0600

    Done: SkeletorUi shared component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,21 +1,7 @@
-import { useState } from "react";
-import PaginationControlled from "../components/shared/PaginationControlled";
-
 const App = () => {
-    const [page, setPage] = useState(1);
-    const handleChange = (event, value) => {
-      setPage(value);
-    };
-  
-
     return (
         <div>
             <h1>Main App</h1>
-            <PaginationControlled
-                handleChange={handleChange}
-                page={page}
-                count={20}
-            />
         </div>
     );
 };
