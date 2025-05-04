5d10d33f5b9f4e09fea82334aa02956a728d3dfe
shahadat hossen <hossantopu@gmail.com>
2025-05-04 18:38:59 +0600

    Done: ImageField ui component

--- src/components/ui/ImageField.jsx
+++ src/components/ui/ImageField.jsx
@@ -1,10 +1,22 @@
+import { Box } from "@mui/material";
 
 
-const ImageField = () => {
+const ImageField = ({title,img,width='100px',height='100px',rounded='',sx={}}) => {
     return (
-        <div>
+        <Box
+            component='img'
+            srcSet={`${img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${img}?w=248&fit=crop&auto=format`}
+            alt={title}
+            loading="lazy"
+            width={width}
+            height={height}
+            sx={{
+                borderRadius: rounded,
+                ...sx
+            }}
             
-        </div>
+          />
     );
 };
 
